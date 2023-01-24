a49a8f21c61c35256a5057a2447ec1b4ae1977aa
Jens Ahrens <ahrjens@gmail.com>
2023-01-24 14:33:10 +0100

    Remove redundant --no-websocket-server from shell scripts

--- data_linked_in_ssr_manual/start_ssr_4_times.sh
+++ data_linked_in_ssr_manual/start_ssr_4_times.sh
@@ -4,16 +4,16 @@
 
 
 # Start SSR 1, loudspeakers 1-2
-ssr-brs --fudi-server=1147 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_1-2 loudspeakers_1-2.asd &
+ssr-brs --fudi-server=1147 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_1-2 loudspeakers_1-2.asd &
 
 # Start SSR 2, loudspeakers 3-4
-ssr-brs --fudi-server=1148 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_3-4 loudspeakers_3-4.asd &
+ssr-brs --fudi-server=1148 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_3-4 loudspeakers_3-4.asd &
 
 # Start SSR 3, loudspeakers 4-5
-ssr-brs --fudi-server=1149 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_5-6 loudspeakers_5-6.asd &
+ssr-brs --fudi-server=1149 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_5-6 loudspeakers_5-6.asd &
 
 # Start SSR 4, loudspeakers 6-7
-ssr-brs --fudi-server=1150 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_7-8 loudspeakers_7-8.asd &
+ssr-brs --fudi-server=1150 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_7-8 loudspeakers_7-8.asd &
 
 sleep 5 
 
--- data_linked_in_ssr_manual/start_ssr_4_times_macos_app.sh
+++ data_linked_in_ssr_manual/start_ssr_4_times_macos_app.sh
@@ -5,16 +5,16 @@
 echo IMPORTANT: Make sure that you adapt the paths to the asdf files inside this shell script to your local setup!
 
 # Start SSR 1, loudspeakers 1-2
-open -n -a SoundScapeRenderer --args --brs "--fudi-server=1147 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_1-2 ~/Documents/misc/ssr/loudspeakers_1-2.asd"
+open -n -a SoundScapeRenderer --args --brs "--fudi-server=1147 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_1-2 ~/Documents/misc/ssr/loudspeakers_1-2.asd"
 
 # Start SSR 2, loudspeakers 3-4
-open -n -a SoundScapeRenderer --args --brs "--fudi-server=1148 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_3-4 ~/Documents/misc/ssr/loudspeakers_3-4.asd"
+open -n -a SoundScapeRenderer --args --brs "--fudi-server=1148 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_3-4 ~/Documents/misc/ssr/loudspeakers_3-4.asd"
 
 # Start SSR 3, loudspeakers 4-5
-open -n -a SoundScapeRenderer --args --brs "--fudi-server=1149 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_5-6 ~/Documents/misc/ssr/loudspeakers_5-6.asd"
+open -n -a SoundScapeRenderer --args --brs "--fudi-server=1149 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_5-6 ~/Documents/misc/ssr/loudspeakers_5-6.asd"
 
 # Start SSR 4, loudspeakers 6-7
-open -n -a SoundScapeRenderer --args --brs "--fudi-server=1150 --no-websocket-server --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_7-8 ~/Documents/misc/ssr/loudspeakers_7-8.asd"
+open -n -a SoundScapeRenderer --args --brs "--fudi-server=1150 --input-prefix=XXX:XXX --output-prefix=YYY:YYY --name=loudspeakers_7-8 ~/Documents/misc/ssr/loudspeakers_7-8.asd"
 
 sleep 5 
 
